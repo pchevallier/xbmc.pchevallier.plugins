efe599f15d18903fc6dc6dc4cc5dd6eef82a53f1
pchevallier <philippe@chevallier.net>
2023-12-30 13:00:35 -0500

    Update

--- addons_xml_generator.py
+++ addons_xml_generator.py
@@ -19,11 +19,13 @@
 # *
 # *  Based on code by j48antialias:
 # *  https://anarchintosh-projects.googlecode.com/files/addons_xml_generator.py
-
+ 
 """ addons.xml generator """
 
+from shutil import make_archive,copyfile
 import os
 import sys
+import re
 
 # Compatibility with 3.0, 3.1 and 3.2 not supporting u"" literals
 if sys.version < '3':
@@ -33,7 +35,7 @@ if sys.version < '3':
 else:
     def u(x):
         return x
-
+ 
 class Generator:
     """
         Generates a new addons.xml file from each addons addon.xml file
@@ -46,17 +48,17 @@ class Generator:
         self._generate_md5_file()
         # notify user
         print("Finished updating addons xml and md5 files")
-
+ 
     def _generate_addons_file( self ):
         # addon list
-        addons = os.listdir( "./" )
+        addons = os.listdir( "." )
         # final addons text
         addons_xml = u("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<addons>\n")
         # loop thru and add each addons addon.xml file
         for addon in addons:
             try:
                 # skip any file or .svn folder or .git folder
-                if ( not os.path.isdir( addon ) or addon == ".svn" or addon == ".git" ): continue
+                if ( not os.path.isdir( addon ) or addon == ".svn" or addon == ".git" or addon == "nbproject" or addon == "zip"): continue
                 # create path
                 _path = os.path.join( addon, "addon.xml" )
                 # split lines for stripping
@@ -74,6 +76,29 @@ class Generator:
                         addon_xml += line.rstrip() + "\n"
                 # we succeeded so add to our final addons.xml text
                 addons_xml += addon_xml.rstrip() + "\n\n"
+                # get plugin version
+                fcontent = open( _path, "r" ).read()
+                zversion = re.compile('<addon id=".+?" name=".+?" version="(.+?)"', re.DOTALL).findall(fcontent)
+                addonname = re.compile('<addon id="(.+?)" name="', re.DOTALL).findall(fcontent)
+                # zip dir
+                zdir = os.path.join(".", "zip")
+                # zipped addon dir
+                zaddondir = os.path.join(".", "zip", addonname[0])
+                # #
+                if not os.path.exists(zaddondir):
+                    os.mkdir(zaddondir)
+                if os.path.isfile(os.path.join(zaddondir, "changelog.txt")):
+                    os.remove(os.path.join(zaddondir, "changelog.txt"))
+                if os.path.isfile(os.path.join(zaddondir, "changelog-"+ zversion[0] +".txt")):
+                    os.remove(os.path.join(zaddondir, "changelog-"+ zversion[0] +".txt"))
+                if os.path.isfile(os.path.join(zaddondir, addonname[0] + "-" + zversion[0] +".zip")):
+                    os.remove(os.path.join(zaddondir, addonname[0] +"-"+ zversion[0] +".zip"))
+                copyfile(os.path.join(addon, "changelog.txt"),os.path.join(zaddondir, "changelog.txt"))
+                copyfile(os.path.join(addon, "changelog.txt"),os.path.join(zaddondir, "changelog-"+ zversion[0] +".txt"))
+                # create zipfile
+                archive_name = os.path.join(zaddondir, addonname[0] +"-"+ zversion[0])
+                make_archive(archive_name, 'zip', ".", addonname[0])
+                #make_zipfile(os.path.join(zaddondir, addon + "-" + zversion[0]),addon)
             except Exception as e:
                 # missing or poorly formatted addon.xml
                 print("Excluding %s for %s" % ( _path, e ))
@@ -81,7 +106,7 @@ class Generator:
         addons_xml = addons_xml.strip() + u("\n</addons>\n")
         # save file
         self._save_file( addons_xml.encode( "UTF-8" ), file="addons.xml" )
-
+ 
     def _generate_md5_file( self ):
         # create a new md5 hash
         try:
@@ -90,14 +115,14 @@ class Generator:
         except ImportError:
             import hashlib
             m = hashlib.md5( open( "addons.xml", "r", encoding="UTF-8" ).read().encode( "UTF-8" ) ).hexdigest()
-
+ 
         # save file
         try:
             self._save_file( m.encode( "UTF-8" ), file="addons.xml.md5" )
         except Exception as e:
             # oops
             print("An error occurred creating addons.xml.md5 file!\n%s" % e)
-
+ 
     def _save_file( self, data, file ):
         try:
             # write data to the file (use b for Python 3)
@@ -106,7 +131,18 @@ class Generator:
             # oops
             print("An error occurred saving %s file!\n%s" % ( file, e ))
 
-
+    def make_zipfile(output_filename, source_dir):
+        relroot = os.path.abspath(os.path.join(source_dir, os.pardir))
+        with zipfile.ZipFile(output_filename, "w", zipfile.ZIP_DEFLATED) as zip:
+            for root, dirs, files in os.walk(source_dir):
+                # add directory (needed for empty dirs)
+                zip.write(root, os.path.relpath(root, relroot))
+                for file in files:
+                    filename = os.path.join(root, file)
+                    if os.path.isfile(filename): # regular files only
+                        arcname = os.path.join(os.path.relpath(root, relroot), file)
+                        zip.write(filename, arcname)
+ 
 if ( __name__ == "__main__" ):
     # start
     Generator()
